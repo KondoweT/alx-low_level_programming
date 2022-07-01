684d9dfd8c5a12d51246f0a9282df300cbfc4230
KondoweT <mukukakondowe@gmail.com>
2022-07-01 03:16:45 +0200

    Betty corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
-	return (0);
+char str[] = "\"Programming is like building a multilingual puzzle";
+
+puts(str);
+
+return (0);
 }
