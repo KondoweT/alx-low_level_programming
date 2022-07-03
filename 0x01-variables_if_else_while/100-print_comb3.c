f984f121031cf081e9adc54fd2b6de05d3b5286b
KondoweT <mukukakondowe@gmail.com>
2022-07-04 00:40:34 +0200

    Prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,25 +3,25 @@
 
 /**
  * main - prints all possible different combinations of two digits
- * Returns: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int i, j;
 
-	/* Nest for loop in for loop to generate comparison numbers */
-	for (i = 48; i < 57; i++)
+	i = 48;
+	/* Use a for loop to compare values of i and j */
+	for (j = 49; j <= 57; j++)
 	{
-		for (j = 49; j <= 57 ; j++)
+		putchar(i);
+		putchar(j);
+		if (i < 56 && j < 57)
 		{
-			putchar(i);
-			putchar(j);
-			if ( i < 56 && j < 57)
-			{
-				putchar(44);
-				putchar(32);
-			}
+			putchar(44);
+			putchar(32);
 		}
+
+		i++;
 	}
 
 	/* Print new line */
