4c656aa4bf19e2a22dd54dec46bfab32badfe8d6
KondoweT <mukukakondowe@gmail.com>
2022-07-03 22:08:22 +0200

    Print numbers 0 - 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Print 0 - 9 using putchar()
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	/* Print the numbers */
+	for (num = 30; num <= 39; num++)
+	{
+		putchar(num);
+	}
+
+	/* Print new line */
+	putchar('\n');
+
+	return (0);
+}
