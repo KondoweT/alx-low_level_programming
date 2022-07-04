7fdd9676e952d74675af79d1d3437ca368441520
KondoweT <mukukakondowe@gmail.com>
2022-07-04 22:46:59 +0200

    Prints combination of two two digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,13 +16,13 @@ int main(void)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
-			putchar(44);
+			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (!(i == 98 && j == 99))
 			{
-			putchar(32);
-			putchar(44);
+			putchar(',');
+			putchar(' ');
 			}
 		}
 	}
