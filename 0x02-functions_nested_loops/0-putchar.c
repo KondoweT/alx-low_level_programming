4d07fc94a85f480f5eb77aaac45d0d9a4ba87110
KondoweT <mukukakondowe@gmail.com>
2022-07-06 22:39:06 +0200

    Program prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* add header file for _putchar */
+#include "_putchar.c"
+
+/**
+ * main - Prints _putchar followed by a new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str;
+
+	/* Store _putchar in string variable */
+	str = "_putchar";
+
+	/* call the _putchar() function in _putchar.c */
+	_putchar(str);
+
+	return (0);
+}
