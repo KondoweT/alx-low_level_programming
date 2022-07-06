525bb898b545477b8bc16dbc802149af0eb2de02
KondoweT <mukukakondowe@gmail.com>
2022-07-07 00:16:08 +0200

    Check if character is letter of alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character checked if it is a letter in upper or lower case
+ *
+ * Return: 1 if letter, lowercase or uppercase and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	int num;
+
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		num = 1;
+	else
+		num = 0;
+	
+	return (num);	
+}
