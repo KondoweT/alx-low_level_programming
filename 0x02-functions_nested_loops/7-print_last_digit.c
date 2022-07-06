9f2ddad3bc0733d2f45bc248a772725e91870048
KondoweT <mukukakondowe@gmail.com>
2022-07-07 00:49:16 +0200

    Print last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+/**
+ * print_last_digit - Returns the last digit of number given
+ * @num: The number to find last digit
+ * Return: The last digit on num
+ */
+
+
+int print_last_digit(int num)
+{
+	int lastD;
+
+	lastD = num % 10;
+
+	return (lastD);
+}
