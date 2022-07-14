137f111e9e2ba315d3bb39902a4774d894de88f4
KondoweT <mukukakondowe@gmail.com>
2022-07-15 00:12:28 +0200

    Return length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to count length
+ * Return: the size of string
+ */
+
+int _strlen(char *s)
+{
+	char string[];
+	int size;
+
+	string[] = *s;
+	size = sizeof(string[]);
+
+	return (size);
+}
